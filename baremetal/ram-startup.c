4ca3f2a43192dbfac7b68d89766e98bf5cd519db
Jeff Kent <jeff@jkent.net>
2020-05-07 20:32:08 -0500

    untested ram startup, improve linker script

---
+++ baremetal/ram-startup.c
@@ -0,0 +1,44 @@
+/* vim: set ts=4 sw=4 noexpandtab
+ *
+ * Copyright (C) 2020 Jeff Kent <jakent@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <asm/attributes.h>
+#include <asm/io.h>
+#include <mach/alive.h>
+
+#if defined (CONFIG_BAREMETAL_LATCH_POWER)
+static void startup_latch_power(void);
+#endif
+void main(void);
+
+EARLY_CODE NAKED void startup(void)
+{
+	startup_latch_power();
+	main();
+}
+
+#if defined(CONFIG_BAREMETAL_LATCH_POWER)
+EARLY_CODE static void startup_latch_power(void)
+{
+	writel(ALIVE_PWRGATEREG_NPOWERGATING, ALIVE_BASE + ALIVE_PWRGATEREG);
+	writel(0, ALIVE_BASE + ALIVE_GPIORSTREG);
+	writel(ALIVE_GPIO_VDDPWRONRST, ALIVE_BASE + ALIVE_GPIOSETREG);
+	writel(0, ALIVE_BASE + ALIVE_GPIOSETREG);
+	writel(0, ALIVE_BASE + ALIVE_PWRGATEREG);
+}
+#endif
